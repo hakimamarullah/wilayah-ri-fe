cdbc003b607ceaed46218a188a0c8cbca8684982
hakimamarullah <hakimamarullah@gmail.com>
2024-09-14 20:56:26 +0700

    Fix allowed origin when forwarding ports

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  output: "standalone",
+  serverActions: { allowedOrigins: ["*", "localhost:3001"], }
 };
 
 export default nextConfig;
