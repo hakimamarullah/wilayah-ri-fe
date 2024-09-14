acf3b805ac2d5f92d9d67de4ce63baac7b745e0b
hakimamarullah <hakimamarullah@gmail.com>
2024-09-14 21:10:57 +0700

    Try config header

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  serverActions: { allowedOrigins: ["*", "localhost:3000"], }
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Adjust this to match your allowed origins
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: '*',
+          },
+          {
+            key: 'X-Forwarded-Host',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
