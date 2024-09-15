e8fd9e85b7931ff7f2749e4da381023661a7db36
hakimamarullah <hakimamarullah@gmail.com>
2024-09-15 11:54:05 +0700

    Fix allowed forwarded host

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "standalone",
   experimental: {
     serverActions: {
-      allowedOrigins: ["*", "localhost:3000", "s6kt0pdb-3000.asse.devtunnels.ms"]
+      allowedOrigins: ["localhost:3000", "localhost:9099"]
     }
   }
 };
